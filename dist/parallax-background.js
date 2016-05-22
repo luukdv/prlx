ef13e0822e12a18038cc8babc4a5b0a7cd023928
Luuk de Vlieger <luuk@trendwerk.nl>
2016-05-22 21:23:56 +0200

    Initial commit

---
+++ dist/parallax-background.js
@@ -0,0 +1,125 @@
+'use strict';
+
+jQuery(function($) {
+  /**
+   * Check if device is running on iOS7,
+   * because DOM painting is paused during scroll events
+   */
+  if(/iP(ad|hone|od).*OS 7.*/.test(navigator.userAgent)) {
+    return;
+  }
+
+  /**
+   * Check for RAF support
+   */
+  if(! window.requestAnimationFrame) {
+    return;
+  }
+
+  var $elements = $('.example');
+
+  if(! $elements.length) {
+    return;
+  }
+
+  var windowElement = $(window);
+  var windowHeight = windowElement.height();
+  var items = [];
+
+  /**
+   * Initialize function,
+   * and call again on window resize to support element/window size changes
+   */
+  init();
+  windowElement.resize(init);
+
+  function init() {
+    windowHeight = windowElement.height();
+
+    $elements.each(function(key) {
+      var $element = $(this);
+      var $image = $element.find('.image');
+
+      /**
+       * Support initially hidden elements to avoid unstyled flash
+       */
+      $image.show();
+
+      var height = $element.height();
+
+      /**
+       * Check the available space for parallax movements,
+       * and continue to next element when there is none
+       */
+      var parallaxSpace = $image.height() - height;
+
+      if(parallaxSpace === 0) {
+        return;
+      }
+
+      var offset = $element.offset().top;
+
+      /**
+       * Calculate some necessary variables
+       */
+      items[key] = {
+        $image: $image,
+        distanceToVisible: offset - windowHeight,
+        height: height,
+        offset: offset,
+        parallaxSpace: parallaxSpace
+      };
+
+      /**
+       * Check how big the space is where parallaxing should take place
+       */
+      items[key].scrollSpace = height + windowHeight;
+    });
+  }
+
+  requestAnimationFrame(parallax);
+
+  function parallax() {
+    var scrollTop = windowElement.scrollTop();
+
+    /**
+     * Separate calculations for each element
+     */
+    $.each(items, function(key, item) {
+      /**
+       * Check when element is in and out of sight,
+       * for precise animations and avoiding execution when element is not visible
+       */
+      var inSight = scrollTop >= item.distanceToVisible;
+      var outOfSight = scrollTop >= (item.height + item.offset);
+
+      if(inSight && ! outOfSight) {
+        var scrollAmount = scrollTop - item.distanceToVisible;
+
+        /**
+         * Support negative amounts,
+         * to make sure parallax is centered at the middle of the viewport
+         */
+        scrollAmount = scrollAmount - (item.scrollSpace / 2);
+
+        /**
+         * Calculate pixel amount to be animated,
+         * and round off unnecessary precise amount for better performance
+         */
+        var animatePerPixel = item.parallaxSpace / item.scrollSpace;
+        var translate = (scrollAmount * animatePerPixel).toFixed(2);
+
+        /**
+         * Invert amount for more parallax effect
+         */
+        translate = translate * -1;
+
+        item.$image.css({
+          'transform': 'translate3d(0, ' + translate + 'px, 0)'
+        });
+      }
+    });
+
+    requestAnimationFrame(parallax);
+  }
+});
