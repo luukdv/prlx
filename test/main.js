b2b62fcefa000870fb9cb5978730d199ea17683d
Luuk <info@luuk.site>
2017-01-22 12:38:32 +0100

    :white_check_mark: Setup test suite

--- test/main.js
+++ test/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+import * as phantom from 'phantom';
+import test from 'ava';
+
+const page = new Promise(resolve => {
+  phantom.create()
+    .then(p => p.createPage())
+    .then(p => {
+      resolve(p.evaluate(function() {
+        return this;
+      }));
+    });
+});
