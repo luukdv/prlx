9204a9e941293d3f6421bf8429c2a797b44a783b
Luuk <info@luuk.site>
2017-01-22 15:09:09 +0100

    :sparkles: Module export

--- src/prlx.js
+++ src/prlx.js
@@ -1,1 +1,17 @@
 'use strict';
+
+((root) => {
+  class Parallax {
+    constructor(nodes) {
+      this.nodes = nodes;
+    }
+  }
+
+  const exp = item => new Parallax(item);
+
+  if (typeof module === 'object') {
+    module.exports = exp;
+  } else {
+    root.prlx = exp;
+  }
+})(this);
